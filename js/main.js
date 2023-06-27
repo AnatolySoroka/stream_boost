93b21fe7495f0134af3ec0478453ceeb67bd1969
AnatolySoroka <toliksoroka66@gmail.com>
2023-06-27 13:23:49 +0300

    edits

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ window.addEventListener('load', function () {
                 }
 
                 if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                    animItem.className += ' animate';
+                    animItem.classList.add('animate');
                 } else {
                     if (!animItem.classList.contains('animate-no-hide')) {
                         animItem.classList.remove('animate');
